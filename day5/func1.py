2d80c5c403999a8b0cde64ab982c5a49de5272c4
user <user@DESKTOP-8JQ4MJ8>
2026-05-11 12:46:14 +0900

    반복문 연습

---
+++ day5/func1.py
@@ -0,0 +1,7 @@
+#함수
+def add(a,b):
+    return a+b
+n1=int(input("숫자1 입력: "))
+n2=int(input("숫자2 입력: "))    
+
+print(add(n1,n2))
--- day5/loop.py
+++ day5/loop.py
@@ -11,3 +11,17 @@ print(total)
 #       continue #조건으로 이동
 #     total+=i
 # print(total)
+
+import sys
+user_pass = input("패스워드를 입력하시오(영어3자): ")
+password = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u','v', 
+            'w', 'x', 'y', 'z',]
+for letter1 in password:
+        for letter2 in password:
+            for letter3 in password:
+                guess = letter1+letter2+letter3
+                print(guess)
+                if guess == user_pass:
+                    print("당신의 패스워드는 "+guess)
+                    sys.exit() #중지
