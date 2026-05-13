0a36afb87dec04c60ece426049785ddcb4845aeb
olzlkor <7714jihyo@gmail.com>
2026-05-13 15:52:24 +0900

    Add files via upload

---
+++ day6/ex01_main.py
@@ -0,0 +1,13 @@
+# import ex01_func
+# ex01_func.hello()
+
+# from ex01_func import *
+# hello()
+
+# import ex01_func as f1 (as=alias 별칭)
+# f1.hello()
+
+from ex01_func import hi
+hi()
+#hello() 오류
+
